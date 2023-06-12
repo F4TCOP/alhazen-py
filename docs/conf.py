28a4a31c6e128698b4f733ca4ee0e99e80f98a29
LordAndraXXXus <andrakszusz@gmail.com>
2023-06-12 13:36:04 +0200

    trying to fix Main class bug

--- docs/conf.py
+++ docs/conf.py
@@ -2,7 +2,7 @@ import sys
 import os
 
 sys.path.insert(0, os.path.abspath('../src'))
-
+sys.path.insert(0, os.path.abspath('../../src'))
 #project = 'alhazen-py'
 html_title = 'Alhazen'
 html_theme = 'sphinx_book_theme'
